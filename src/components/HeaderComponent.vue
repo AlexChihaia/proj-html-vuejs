6d7801d55989ca6a603d8a369b1f9c21496548f2
AlexChihaia <alexchihaia84@gmail.com>
2023-12-19 17:21:00 +0100

    feat: header title

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -48,6 +48,12 @@ export default {
             </ul>
         </div>
 
+        <div class="box">
+            <h2>Damon Vaugh</h2>
+            <h4>Best selling author and the most influential public intellectual in the western world right now.</h4>
+            <p>- The new york times</p>
+        </div>
+
         <div class="container-small">
             <div class="row">
                 <div class="col"><img src="../assets/img/" alt=""></div>
@@ -59,6 +65,7 @@ export default {
 </template>
 <style scoped lang="scss">
 @use '../assets/scss/partials/typography.scss' as *;
+@use '../assets/scss/partials/variables.scss' as *;
 
 header {
     background-image: url(../assets/img/footer-3.jpg);
@@ -74,6 +81,25 @@ header {
         display: flex;
         justify-content: space-between;
         padding: 0 110px;
+    }
+
+    .box {
+        padding: 0 230px;
+        width: 1180px;
+
+        h2 {
+            font-family: 'Kristi', cursive;
+            color: $warn-color;
+
+            font-size: 6rem;
+
+        }
+
+        h4 {
+            font-family: 'DM Serif Display', serif;
+            color: #ffff;
+            font-size: 3rem;
+        }
     }
 
     .container-small {
