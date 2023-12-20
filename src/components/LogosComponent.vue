f14b9d2455f8c79205f5943621ad607979adc05f
AlexChihaia <alexchihaia84@gmail.com>
2023-12-20 20:33:31 +0100

    feat: logos component

--- src/components/LogosComponent.vue
+++ src/components/LogosComponent.vue
@@ -3,5 +3,35 @@ export default {
     name: 'Logos'
 }
 </script>
-<template></template>
-<style scoped lang="scss"></style>
+<template>
+    <div class="row">
+        <div class="logo">
+            <a href="#"><img src="../assets/img/ebay-logotan-2x-200x66.png" alt=""></a>
+        </div>
+        <div class="logo">
+            <a href="#"><img src="../assets/img/audible-logotan-2x-200x66.png" alt=""></a>
+        </div>
+        <div class="logo">
+            <a href="#"><img src="../assets/img/bb-logotan-2x-200x66.png" alt=""></a>
+        </div>
+        <div class="logo">
+            <a href="#"><img src="../assets/img/kindlefire-logotan-2x-200x66.png" alt=""></a>
+        </div>
+
+    </div>
+</template>
+<style scoped lang="scss">
+@use '../assets/scss/partials/mixins.scss' as *;
+@use '../assets/scss/partials/variables.scss' as *;
+
+.row {
+    @include row;
+    background-color: #f3ece3;
+    justify-content: center;
+    padding: 65px 0;
+
+    .logo {
+        padding: 0 70px;
+    }
+}
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -22,6 +22,7 @@ export default {
 <template>
     <section>
         <About />
+        <Logos />
     </section>
     <section>
 
